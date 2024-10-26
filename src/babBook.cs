3ddfa6ed957915467e545663c1a45b1f78122c60
J Melg <rjmzelaya@gmail.com>
2024-10-26 17:16:20 -0600

    First commit

---
+++ src/babBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babylon.src {
+    //Class that holds the Pages that are contained in a book
+    //Each page has a defined amount of lines with a lenght of 85
+    public static class Page {
+        //Each page contains 8 lines
+        private static String[] lines = new String[8]; 
+        //Length per line in characters
+        private const int Length = 80;
+    }
+
+    class babBook {
+        //properties
+        private string title; //for the book's spine
+        //number of pages per book
+        private const int pagesN = 410;
+        public
+        
+    }
+}
